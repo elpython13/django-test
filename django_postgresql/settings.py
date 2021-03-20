3974bbef19f77fe2fe17d463cf702686cc63fabf
Kristian Ellis <kristian.w.ellis@gmail.com>
2021-03-20 22:00:40 +0000

    third commit

--- django_postgresql/settings.py
+++ django_postgresql/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom
+    'travello.apps.TravelloConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,11 @@ WSGI_APPLICATION = 'django_postgresql.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_postgresql',
+        'USER': 'postgres',
+        'PASSWORD': 'oxford123',
+        'HOST': 'localhost',
     }
 }
 
@@ -123,4 +129,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- travello/views.py
+++ travello/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import Destination
 
 def index(request):
-    return render(request, 'index.html')
+
+
+    dests = Destination.objects.all()
+
+    return render(request, 'index.html', {'dests': dests})
+
+
 
